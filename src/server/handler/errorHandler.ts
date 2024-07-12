cd48bc793a178569e4a77d230b7f724e3f73b680
Edwin Ortiz <edarcode@gmail.com>
2024-07-12 16:53:49 -0500

    refactor errHandler

--- src/server/handler/errorHandler.ts
+++ src/server/handler/errorHandler.ts
@@ -2,19 +2,19 @@ import { ERROR_CODES_DB } from "../../db/errCodes";
 import { ErrorHandler } from "../../types";
 
 export const errorHandler: ErrorHandler = (error, _req, res, _next) => {
-  const { status = 500, name, code } = error;
+  const { name } = error;
 
   if (name === "PrismaClientKnownRequestError") {
+    const { code } = error;
     const keyCode = code as keyof typeof ERROR_CODES_DB;
     const errCode = ERROR_CODES_DB[keyCode];
-    const status = errCode?.status || 400;
     const msg = errCode?.msg || error;
-    return res.status(status).json({ msg });
+    return res.status(400).json({ msg });
   }
 
   if (name === "ZodError") {
     return res.status(400).json(error);
   }
 
-  return res.status(status).json(error);
+  return res.status(500).json(error);
 };
