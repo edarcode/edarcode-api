0f19869ce47ae305dfd1b35cbbf794c25bfcef3e
Edwin Ortiz <edarcode@gmail.com>
2024-07-12 01:52:00 -0500

    refactor

---
+++ src/db/errCodes.ts
@@ -0,0 +1,3 @@
+export const ERROR_CODES_DB = {
+  P2025: { status: 400, msg: "El usuario no existe" },
+} as const;
--- src/server/errorHandler.ts
+++ src/server/errorHandler.ts
@@ -1,17 +1,15 @@
+import { ERROR_CODES_DB } from "../db/errCodes";
 import { ErrorHandler } from "../types";
 
 export const errorHandler: ErrorHandler = (error, _req, res, _next) => {
   const { status = 500, name, code } = error;
 
   if (name === "PrismaClientKnownRequestError") {
-    const status = ERRORS_DB[code as keyof typeof ERRORS_DB].status;
-    const msg = ERRORS_DB[code as keyof typeof ERRORS_DB].msg;
+    const errCode = ERROR_CODES_DB[code as keyof typeof ERROR_CODES_DB];
+    const status = errCode.status;
+    const msg = errCode.msg;
     return res.status(status).json({ msg });
   }
 
   return res.status(status).json(error);
 };
-
-const ERRORS_DB = {
-  P2025: { status: 400, msg: "El usuario no existe" },
-} as const;
