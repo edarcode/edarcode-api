2b6c723f0d07288f2f27be21c6fa5a8b7e20aece
Edwin Ortiz <edarcode@gmail.com>
2024-07-12 01:26:06 -0500

    agg errHandler

--- src/server/errorHandler.ts
+++ src/server/errorHandler.ts
@@ -1,7 +1,17 @@
 import { ErrorHandler } from "../types";
 
 export const errorHandler: ErrorHandler = (error, _req, res, _next) => {
-  const status = error?.status || 500;
-  const message = error?.message || error;
-  res.status(status).json(message);
+  const { status = 500, name, code } = error;
+
+  if (name === "PrismaClientKnownRequestError") {
+    const status = ERRORS_DB[code as keyof typeof ERRORS_DB].status;
+    const msg = ERRORS_DB[code as keyof typeof ERRORS_DB].msg;
+    return res.status(status).json({ msg });
+  }
+
+  return res.status(status).json(error);
 };
+
+const ERRORS_DB = {
+  P2025: { status: 400, msg: "El usuario no existe" },
+} as const;
--- src/server/notFoundHandler.ts
+++ src/server/notFoundHandler.ts
@@ -1,5 +1,5 @@
 import { NotFoundHandler } from "../types";
 
 export const notFoundHandler: NotFoundHandler = (_req, res) => {
-  res.json({ msg: "Oops! Route not found 👀" });
+  res.status(404).json({ msg: "Oops! Route not found 👀" });
 };
