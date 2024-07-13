1c7ff3644ff9563740ac7af73a171882d2318b25
Edwin Ortiz <edarcode@gmail.com>
2024-07-13 18:45:13 -0500

    refactor

--- src/middleware/verifyToken.ts
+++ src/middleware/verifyToken.ts
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
-import { SECRET_JWT } from "../constant/jwt";
+
+import { JWT } from "../constant/jwt";
 import { EdarErr } from "../error/EdarErr";
 import { Middleware } from "../types";
 
@@ -8,7 +9,7 @@ export const verifyToken: Middleware = (req, res, next) => {
     const token = req.headers.authorization;
     if (!token) throw new EdarErr(401, "Unauthorized token");
 
-    jwt.verify(token, SECRET_JWT as string, (err, infoToken) => {
+    jwt.verify(token, JWT.secret as string, (err, infoToken) => {
       if (err) throw new EdarErr(403, "Unauthorized token");
       res.locals = { ...res.locals, infoToken };
       next();
