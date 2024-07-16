cce7356238110eb7873a7557d8bfff9c35dbdbf1
Edwin Ortiz <edarcode@gmail.com>
2024-07-16 12:47:31 -0500

    separando modulos de user para boss y client

--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -2,11 +2,10 @@ import { Role } from "@prisma/client";
 import { Router } from "express";
 import { verifyToken } from "../middleware/verifyToken";
 import { verifyTokenRole } from "../middleware/verifyTokenRole";
-
 import { bossRouter } from "./boss/bossRouter";
-import { authRouter } from "./client/auth/authRouter";
+import { clientRouter } from "./client/clientRouter";
 
 export const userRouter = Router();
 
 userRouter.use("/boss", verifyToken, verifyTokenRole(Role.BOSS), bossRouter);
-userRouter.use("/auth", authRouter);
+userRouter.use("/client", clientRouter);
