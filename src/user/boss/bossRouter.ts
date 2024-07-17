a527c7e7c2d499b3c8024869b127bdd9646ae512
Edwin Ortiz <edarcode@gmail.com>
2024-07-17 10:25:12 -0500

    reestructurando modulos

--- src/user/boss/bossRouter.ts
+++ src/user/boss/bossRouter.ts
@@ -1,4 +1,7 @@
+import { Role } from "@prisma/client";
 import { Router } from "express";
+import { verifyToken } from "../../middleware/verifyToken";
+import { verifyTokenRole } from "../../middleware/verifyTokenRole";
 import { createUserRouter } from "./create-user/createUserRouter";
 import { deleteUserRouter } from "./delete-user/deleteUserRouter";
 import { getUsersRouter } from "./get-users/getUsersRouter";
@@ -6,6 +9,7 @@ import { modifyUserRouter } from "./modify-user/modifyUserRouter";
 
 export const bossRouter = Router();
 
+bossRouter.use(verifyToken, verifyTokenRole(Role.BOSS));
 bossRouter.use("/get", getUsersRouter);
 bossRouter.use("/create", createUserRouter);
 bossRouter.use("/delete", deleteUserRouter);
--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,11 +1,8 @@
-import { Role } from "@prisma/client";
 import { Router } from "express";
-import { verifyToken } from "../middleware/verifyToken";
-import { verifyTokenRole } from "../middleware/verifyTokenRole";
 import { bossRouter } from "./boss/bossRouter";
 import { clientRouter } from "./client/clientRouter";
 
 export const userRouter = Router();
 
-userRouter.use("/boss", verifyToken, verifyTokenRole(Role.BOSS), bossRouter);
+userRouter.use("/boss", bossRouter);
 userRouter.use("/client", clientRouter);
